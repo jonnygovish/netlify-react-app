415a4d580c1ff83328b40b3fc681e03075e4a5a8
Allan <github email address>
2022-08-01 03:38:18 +0300

    commit

--- src/components/Basket/Basket.js
+++ src/components/Basket/Basket.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import React, { useContext } from "react";
 import "./Basket.css";
 import { Link } from "react-router-dom";
--- src/components/Basket/BasketItem.js
+++ src/components/Basket/BasketItem.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Buttons from "../Buttons/Buttons";
 import { Link } from "react-router-dom";
 
